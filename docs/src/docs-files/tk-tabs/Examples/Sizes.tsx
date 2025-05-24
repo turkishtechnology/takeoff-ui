bac7314b4eba294901c815036e02c2379623a647
Ulaş Turan <ulasturan06@hotmail.com>
2025-05-24 20:14:10 +0300

    refactor(docs): Rename code sample variables for consistency

--- docs/src/docs-files/tk-tabs/Examples/Sizes.tsx
+++ docs/src/docs-files/tk-tabs/Examples/Sizes.tsx
@@ -18,7 +18,7 @@ const Type = () => {
     const attributesList = [`size="${size}"`].filter(Boolean);
     const attributes = attributesList.join('\n  ');
 
-    const reactCodeSample = `<TkTabs ${attributes}>
+    const reactCode = `<TkTabs ${attributes}>
   <TkTabsItem label="Tab label" icon="flight">
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate nequequas!</p>
   </TkTabsItem>
@@ -30,7 +30,7 @@ const Type = () => {
   </TkTabsItem>
 </TkTabs>`;
 
-    const vueCodeSample = `<TkTabs ${attributes}>
+    const vueCode = `<TkTabs ${attributes}>
   <TkTabsItem label="Tab label" icon="flight">
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate nequequas!</p>
   </TkTabsItem>
@@ -41,8 +41,8 @@ const Type = () => {
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate nequequas!</p>
   </TkTabsItem>
 </TkTabs>`;
-    setCodeSampleReact(reactCodeSample);
-    setCodeSampleVue(vueCodeSample);
+    setCodeSampleReact(reactCode);
+    setCodeSampleVue(vueCode);
   }, [size]);
 
   const demo = (
