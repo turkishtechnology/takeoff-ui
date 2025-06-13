2c8ca073ebc0e9a13c0fd75554ab5aa4d9e3dbf5
Atakan Erhan <atakanbayil@gmail.com>
2025-06-13 18:09:04 +0300

    feat(tk-rating): Add number type

--- apps/react-app/src/components/Rating.tsx
+++ apps/react-app/src/components/Rating.tsx
@@ -2,11 +2,15 @@ import { TkCard, TkRating } from '@takeoff-ui/react';
 
 function Rating() {
   return (
-    <TkCard>
-      <TkRating readonly={true} value={3} />
-      <TkRating disabled={true} value={3} />
-      <TkRating value={3} />
-    </TkCard>
+    <>
+      <TkCard>
+        <TkRating readonly={true} value={3} />
+        <TkRating disabled={true} value={3} />
+      </TkCard>
+      <div style={{ width: '500px' }}>
+        <TkRating type="number" maxRating={5}></TkRating>
+      </div>
+    </>
   );
 }
 
