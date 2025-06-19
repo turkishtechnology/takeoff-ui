eb3d89c8da016e963e2727d6e8dec154762ed04b
Harun Demir <harundemirp@gmail.com>
2025-06-19 14:41:18 +0300

    fix(tk-tabs): Fixed render when update tab item's label and other prop

--- packages/core/src/components/tk-tabs/tk-tabs-item.tsx
+++ packages/core/src/components/tk-tabs/tk-tabs-item.tsx
@@ -14,28 +14,48 @@ export class TkTabsItem implements ComponentInterface {
    * Icon for tabs item component.
    */
   @Prop() icon?: string | IIconOptions;
+  @Watch('icon')
+  iconChanged() {
+    this.emitUpdate();
+  }
 
   /**
    * Label for the tab item.
    */
   @Prop() label: string;
+  @Watch('label')
+  labelChanged() {
+    this.emitUpdate();
+  }
 
   /**
    * Whether the tab item is disabled.
    */
   @Prop() disabled: boolean;
+  @Watch('disabled')
+  disabledChanged() {
+    this.emitUpdate();
+  }
 
   /**
    * Checks if tab item has badge component or not.
    * @defaultValue false
    */
   @Prop() badged?: boolean = false;
+  @Watch('badged')
+  badgedChanged() {
+    this.emitUpdate();
+  }
 
   /**
    * Sets badge component's label.
    * @deprecated since version 0.0.40. Use 'badgeCount' instead.
    */
   @Prop() badgeLabel?: string;
+  @Watch('badgeLabel')
+  badgeLabelChanged() {
+    this.emitUpdate();
+  }
 
   /**
    * Sets badge component's count.
@@ -43,18 +63,36 @@ export class TkTabsItem implements ComponentInterface {
   @Prop() badgeCount?: number | string;
   @Watch('badgeCount')
   badgeCountChanged() {
-    // Dispatch a custom event that the parent component can listen for
+    this.emitUpdate();
+  }
+
+  /**
+   * Sets tooltip options for the tab item.
+   */
+  @Prop() tooltipOptions?: ITooltipOptions;
+  @Watch('tooltipOptions')
+  tooltipOptionsChanged() {
+    this.emitUpdate();
+  }
+
+  // Dispatch a custom event that the parent component can listen for
+  private emitUpdate() {
     this.el.dispatchEvent(
-      new CustomEvent('badgeUpdated', {
+      new CustomEvent('tk-update', {
         bubbles: true,
         composed: true,
         detail: {
+          label: this.label,
+          icon: this.icon,
+          disabled: this.disabled,
+          badged: this.badged,
           badgeCount: this.badgeCount,
+          badgeLabel: this.badgeLabel,
+          tooltipOptions: this.tooltipOptions,
         },
       }),
     );
   }
-  @Prop() tooltipOptions?: ITooltipOptions;
 
   render() {
     return (
