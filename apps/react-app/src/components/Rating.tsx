2ecc9a6b4011fe2404a23aee4f3f72872f6706ff
Atakan <42473561+atakanbayil@users.noreply.github.com>
2025-06-19 09:26:39 +0300

    feat(tk-rating): Add number type
    * feat(tk-rating): Add number type
    
    * feat(tk-rating): Add condition for hover
    
    * feat(tk-rating): Add uppercase
    
    * docs(tk-rating): Add number type to docs
    
    * feat(tk-rating): Add style and event
    
    * refactor(tk-rating): Refactor rating render number type and styles
    
    ---------
    
    Co-authored-by: Atakan Erhan <atakanbayil@gmail.com>
    Co-authored-by: Harun Demir <harundemirp@gmail.com>

--- apps/react-app/src/components/Rating.tsx
+++ apps/react-app/src/components/Rating.tsx
@@ -1,12 +1,33 @@
 import { TkCard, TkRating } from '@takeoff-ui/react';
+import { useState } from 'react';
 
 function Rating() {
+  const [value, setValue] = useState(3.5);
   return (
-    <TkCard>
-      <TkRating readonly={true} value={3} />
-      <TkRating disabled={true} value={3} />
-      <TkRating value={3} />
-    </TkCard>
+    <>
+      <TkCard>
+        <TkRating
+          type="star"
+          value={value}
+          onTkChange={(e) => setValue(e.detail)}
+        />
+        <TkRating
+          type="heart"
+          value={value}
+          onTkChange={(e) => setValue(e.detail)}
+        />
+        <TkRating
+          type="dot"
+          value={value}
+          onTkChange={(e) => setValue(e.detail)}
+        />
+        <TkRating
+          type="number"
+          value={value}
+          onTkChange={(e) => setValue(e.detail)}
+        />
+      </TkCard>
+    </>
   );
 }
 
