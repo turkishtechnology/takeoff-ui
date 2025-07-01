0887a9b501e0ad7844a7fdb4fa69fd6108d638d1
ht9124 <turkmenogluhuseyin@gmail.com>
2025-07-01 16:43:26 +0300

    feat(tk-phone-input): Phone input docs has been developed

---
+++ docs/src/docs-files/tk-phone-input/Examples/Size.tsx
@@ -0,0 +1,97 @@
+import { TkPhoneInput } from '@takeoff-ui/react';
+import FeatureDemo from '../../../components/FeatureDemo';
+import React, { useState } from 'react';
+
+const Size = () => {
+  const reactCode = `<TkPhoneInput
+    label="Small Input"
+    size="small"
+    value={value}
+    onTkChange={(e) => setValue(e.detail)}
+  />
+  <TkPhoneInput
+    label="Base Input"
+    size="base"
+    value={value1}
+    onTkChange={(e) => setValue1(e.detail)}
+  />
+  <TkPhoneInput
+    label="Large Input"
+    size="large"
+    value={value2}
+    onTkChange={(e) => setValue2(e.detail)}
+  />`;
+
+  const vueCode = `<TkPhoneInput
+    label="Small Input"
+    size="small"
+    v-model="value"
+  />
+  <TkPhoneInput
+    label="Base Input"
+    size="base"
+    v-model="value1"
+  />
+  <TkPhoneInput
+    label="Large Input"
+    size="large"
+    v-model="value2"
+  />`;
+
+  const angularCode = `<tk-phone-input
+    label="Small Input"
+    size="small"
+    [(ngModel)]="value"
+    (tkChange)="setValue($event.detail)"
+  />
+  <tk-phone-input
+    label="Base Input"
+    size="base"
+    [(ngModel)]="value1"
+    (tkChange)="setValue1($event.detail)"
+  />
+  <tk-phone-input
+    label="Large Input"
+    size="large"
+    [(ngModel)]="value2"
+    (tkChange)="setValue2($event.detail)"
+  />`;
+
+  const [value, setValue] = useState();
+  const [value1, setValue1] = useState();
+  const [value2, setValue2] = useState();
+
+  const demo = (
+    <div className="flex flex-col flex-wrap items-start gap-3">
+      <TkPhoneInput
+        label="Small Input"
+        size="small"
+        value={value}
+        onTkChange={(e) => setValue(e.detail)}
+      />
+      <TkPhoneInput
+        label="Base Input"
+        size="base"
+        value={value1}
+        onTkChange={(e) => setValue1(e.detail)}
+      />
+      <TkPhoneInput
+        label="Large Input"
+        size="large"
+        value={value2}
+        onTkChange={(e) => setValue2(e.detail)}
+      />
+    </div>
+  );
+
+  return (
+    <FeatureDemo
+      demo={demo}
+      reactCode={reactCode}
+      vueCode={vueCode}
+      angularCode={angularCode}
+    ></FeatureDemo>
+  );
+};
+
+export default Size;
