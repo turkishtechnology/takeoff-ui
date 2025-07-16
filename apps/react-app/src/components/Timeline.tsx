b718d2bc2b522bef45eb7fc095bfa8edbc660021
ht9124 <turkmenogluhuseyin@gmail.com>
2025-07-16 07:35:54 +0300

    feat(tk-timeline-item): TimelineItem component has been developed (#119)

--- apps/react-app/src/components/Timeline.tsx
+++ apps/react-app/src/components/Timeline.tsx
@@ -1,4 +1,4 @@
-import { TkTimeline } from '@takeoff-ui/react';
+import { TkButton, TkTimeline, TkTimelineItem } from '@takeoff-ui/react';
 
 function Timeline() {
   const items = [
@@ -19,6 +19,52 @@ function Timeline() {
 
       <TkTimeline items={items} />
       <TkTimeline items={items} alternate={false} />
+
+      <TkTimeline orientation="vertical" className="my-4">
+        <TkTimelineItem>
+          <div className="custom-content">
+            <h3>Custom Title</h3>
+            <p>
+              Custom description with <strong>HTML</strong>
+            </p>
+            <img
+              src="https://flagpedia.net/data/flags/h24/tr.webp"
+              alt="Custom image"
+            />
+          </div>
+        </TkTimelineItem>
+
+        <TkTimelineItem>
+          <div className="another-custom-content">
+            <span className="badge">Important</span>
+            <p className="text-red-100">Another custom timeline item</p>
+            <iframe
+              width="300"
+              height="250"
+              src="https://www.youtube.com/embed/LTOSVKGiW18?si=4u6rXsSpxHxjIFj7"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin"
+              allowfullscreen
+            ></iframe>
+          </div>
+        </TkTimelineItem>
+        <TkTimelineItem>
+          <div className="yet-another-custom-content">
+            <h4>Yet Another Item</h4>
+            <p>This is yet another custom timeline item with more content.</p>
+            <TkButton label="Cancel" variant="secondary" type="filled" />
+          </div>
+        </TkTimelineItem>
+        <TkTimelineItem>
+          <div className="final-custom-content">
+            <h5>Final Item</h5>
+            <p>This is the final custom timeline item.</p>
+            <TkButton label="Finish" variant="primary" type="filled" />
+          </div>
+        </TkTimelineItem>
+      </TkTimeline>
     </div>
   );
 }
--- packages/core/src/components/tk-timeline/tk-timeline.tsx
+++ packages/core/src/components/tk-timeline/tk-timeline.tsx
@@ -2,8 +2,13 @@ import { Component, h, Prop, Element, Fragment } from '@stencil/core';
 import { ComponentInterface } from '@stencil/core';
 import classNames from 'classnames';
 import { TimelineItem } from './interfaces';
+
 /**
- * Timeline component to display a sequence of events.
+ * The `TkTimeline` is a component that displays a vertical or horizontal timeline of events.
+ * The `TkTimelineItem` is a helper component used to create customized content within the `TkTimeline` component.
+ * @react `import { TkTimeline, TkTimelineItem } from '@takeoff-ui/react'`
+ * @vue `import { TkTimeline, TkTimelineItem } from '@takeoff-ui/vue'`
+ * @angular `import { TkTimeline, TkTimelineItem } from '@takeoff-ui/angular'`
  */
 @Component({
   tag: 'tk-timeline',
@@ -12,15 +17,18 @@ import { TimelineItem } from './interfaces';
 })
 export class TkTimeline implements ComponentInterface {
   @Element() el: HTMLTkTimelineElement;
+
   /**
    * An array of objects representing the items to display on the timeline.
    * Each object should have at least a `title`. `description` and `date` are optional.
    */
   @Prop() items: TimelineItem[] = [];
+
   /**
    * The orientation of the timeline.
    */
   @Prop() orientation: 'horizontal' | 'vertical' = 'horizontal';
+
   /**
    * Whether to alternate the position of timeline items relative to the line.
    */
@@ -68,12 +76,49 @@ export class TkTimeline implements ComponentInterface {
     );
   }
 
+  private renderSlottedTimelineItems() {
+    const timelineItems = Array.from(this.el.querySelectorAll('tk-timeline-item'));
+
+    return timelineItems.map((item, index) => {
+      const contentPlacement = this.determineContentPlacement(index);
+      const isFirst = index === 0;
+      const isLast = index === timelineItems.length - 1;
+      const itemClasses = classNames('tk-timeline-item', `tk-timeline-item-placement-${contentPlacement}`, {
+        'tk-timeline-item-first': isFirst,
+        'tk-timeline-item-last': isLast,
+        'tk-timeline-item-alternate': this.alternate,
+      });
+
+      // Set attributes on the timeline item for styling purposes
+      item.setAttribute('data-class', `tk-timeline-item-${index}`);
+      item.setAttribute('data-index', index.toString());
+
+      return (
+        <li class={itemClasses}>
+          <div class="tk-timeline-item-content tk-timeline-item-content-start">{contentPlacement === 'start' && <slot name={`item-${index}`}></slot>}</div>
+          <div class="tk-timeline-item-separator">
+            <div class="tk-timeline-item-point"></div>
+            <div class="tk-timeline-item-connector"></div>
+          </div>
+          <div class="tk-timeline-item-content tk-timeline-item-content-end">{contentPlacement === 'end' && <slot name={`item-${index}`}></slot>}</div>
+        </li>
+      );
+    });
+  }
+
+  private hasSlottedContent(): boolean {
+    return this.el.querySelectorAll('tk-timeline-item').length > 0;
+  }
+
   render() {
     const hostClasses = classNames('tk-timeline', `tk-timeline-${this.orientation}`);
+    const hasSlottedItems = this.hasSlottedContent();
 
     return (
       <div class={hostClasses}>
-        <ul class="tk-timeline-items">{this.items.map((item, index) => this.renderTimelineItem(item, index))}</ul>
+        <ul class="tk-timeline-items">
+          {hasSlottedItems ? <Fragment>{this.renderSlottedTimelineItems()}</Fragment> : this.items.map((item, index) => this.renderTimelineItem(item, index))}
+        </ul>
       </div>
     );
   }
