8fdbce4ab58d12f74ca2726673f1cd54acb234f6
Pınar Yalçınduran <104770005+pinaryalcinduran@users.noreply.github.com>
2025-07-17 09:44:58 +0300

    fix(tk-chips): Fix interface props

--- packages/core/src/components/tk-chips/interfaces.ts
+++ packages/core/src/components/tk-chips/interfaces.ts
@@ -1,13 +1,11 @@
 import { IIconOptions } from '../../global/interfaces/IIconOptions';
 
 export interface IChips {
-  label: string;
+  label?: string;
   icon?: string | IIconOptions;
-  disabled?: boolean;
   type?: 'filled' | 'filledlight' | 'outlined' | 'avatar';
   size?: 'large' | 'base' | 'small';
   variant?: 'primary' | 'secondary' | 'neutral' | 'info' | 'success' | 'danger' | 'warning' | 'verified';
   chipStyle?: any;
   labelStyle?: any;
-  autoSelfDestroy: boolean;
 }
