0c62d4f357cafa469c0da929c10a900da70ea0d6
Pınar Yalçınduran <104770005+pinaryalcinduran@users.noreply.github.com>
2025-08-25 14:56:43 +0300

    feat(tk-table): Add header text wrapping option for table columns

--- packages/core/src/components/tk-table/interfaces.ts
+++ packages/core/src/components/tk-table/interfaces.ts
@@ -8,6 +8,8 @@ export interface ITableColumn {
   header: string;
   /** Defines sub heading for the column */
   subHeader?: string;
+  /** Enables wrapping of the header text. If true, header text will wrap to multiple lines when necessary. */
+  headerTextWrap?: boolean;
   /** Defines width for the column */
   width?: string;
   /** Indicates if the column supports sorting */
