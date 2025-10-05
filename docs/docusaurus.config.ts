69fe26e8af33cd68ccbdd9daa20509cad82fbcbf
Harun Demir <harundemirp@gmail.com>
2025-10-05 16:49:15 +0300

    chore(release): Set version to 0.4.0

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -94,7 +94,7 @@ const config: Config = {
         // { to: "/blog", label: "Blog", position: "left" },
         {
           to: 'https://github.com/turkishtechnology/takeoff-ui/releases',
-          label: 'v0.3.2',
+          label: 'v0.4.0',
           position: 'right',
         },
         {
