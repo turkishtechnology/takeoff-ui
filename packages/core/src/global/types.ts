69a35df30a44747e57d91bfc3abe6cb84f4eb5f3
Ulaş Turan <56156254+ulasturann@users.noreply.github.com>
2025-11-12 09:23:28 +0300

    fix(docs): correct CSSStyleProperties in comps' generated API tables (#239)

--- packages/core/src/global/types.ts
+++ packages/core/src/global/types.ts
@@ -1,1 +1,4 @@
+/**
+ * Represents CSS style properties as strings.
+ */
 export type CSSStyleProperties = Partial<{ [K in keyof CSSStyleDeclaration as CSSStyleDeclaration[K] extends string ? K : never]: string }>;
