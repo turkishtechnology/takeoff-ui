1353f99b846908a07ae7aca7fa5be44ea63bdc4b
Pınar Yalçınduran <104770005+pinaryalcinduran@users.noreply.github.com>
2025-12-17 13:16:22 +0300

    fix(tk-table): Use nullish coalescing operator for default row data value in exportFile method (#302)
    * fix(table): use nullish coalescing operator for default row data value in exportFile method
    
    * fix(table): replace logical OR with nullish coalescing operator for cvs and pdf

--- packages/core/src/components/tk-table/tk-table.tsx
+++ packages/core/src/components/tk-table/tk-table.tsx
@@ -417,7 +417,7 @@ export class TkTable implements ComponentInterface {
       autoTable(doc, {
         head: [this.columns.map(col => col.header)], // Başlıkları dinamik olarak ekler
         body: _data.map(
-          row => this.columns.map(col => getNestedValue(row, col.field) || ''), // Her sütunun değerini dinamik olarak alır
+          row => this.columns.map(col => getNestedValue(row, col.field) ?? ''), // Her sütunun değerini dinamik olarak alır
         ),
         theme: 'striped',
         // styles: { halign: 'center', fontSize: 10 },
@@ -450,7 +450,7 @@ export class TkTable implements ComponentInterface {
           _columns
             .filter(col => !options.ignoreColumnsFields?.includes(col.field))
             .forEach(col => {
-              rowData[col.field] = getNestedValue(item, col.field) || '';
+              rowData[col.field] = getNestedValue(item, col.field) ?? '';
             });
 
           return rowData;
@@ -465,7 +465,7 @@ export class TkTable implements ComponentInterface {
       link.click();
     } else if (options.type == 'csv') {
       const headers = this.columns.map(col => col.header).join(',');
-      const rows = _data.map(row => this.columns.map(col => getNestedValue(row, col.field) || '').join(',')).join('\n');
+      const rows = _data.map(row => this.columns.map(col => getNestedValue(row, col.field) ?? '').join(',')).join('\n');
       const csvContent = headers + '\n' + rows;
 
       const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
