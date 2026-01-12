02ed6c1a9f15ba5c903d3b735c31db04161099b9
Harun Demir <harundemirp@gmail.com>
2026-01-12 17:07:48 +0300

    feat(tk-tree-view): Add containerStyle and stepStyle prop to treeview component

--- packages/core/src/components/tk-treeview/tk-tree-view.tsx
+++ packages/core/src/components/tk-treeview/tk-tree-view.tsx
@@ -2,6 +2,7 @@ import { Component, ComponentInterface, Prop, h, State, Element, Event, EventEmi
 import classNames from 'classnames';
 import { ITreeItem } from './interfaces';
 import { IBadgeOptions } from '../../global/interfaces/IBadgeOptions';
+import { CSSStyleProperties } from '../../global/types';
 
 /**
  * The `TkTreeview` component displays hierarchical data in a tree structure with expandable/collapsible nodes.
@@ -103,6 +104,16 @@ export class TkTreeView implements ComponentInterface {
    * **leaf:** selecting a node selects only leaf descendants (and leaf itself if it is a leaf)
    */
   @Prop() selectionStrategy: 'all' | 'leaf' = 'all';
+
+  /**
+   * The style attribute of container element
+   */
+  @Prop() containerStyle?: CSSStyleProperties = null;
+
+  /**
+   * The style attribute of column element for stepper mode
+   */
+  @Prop() stepStyle?: CSSStyleProperties = null;
 
   /**
    * If true, expands all nodes in basic mode.
@@ -746,9 +757,9 @@ export class TkTreeView implements ComponentInterface {
     if (this.mode === 'stepper') {
       const columns = this.getStepperColumns();
       return (
-        <div class={classNames('tk-tree-view', 'stepper')}>
+        <div class={classNames('tk-tree-view', 'stepper')} style={this.containerStyle}>
           {columns.map((column, idx) => (
-            <div class={classNames('tk-tree-view', 'column', 'divided')} key={idx}>
+            <div class={classNames('tk-tree-view', 'column', 'divided')} key={idx} style={this.stepStyle}>
               {column.items.map((item, itemIndex) => this.renderItem(item, column.basePath, itemIndex))}
             </div>
           ))}
@@ -757,7 +768,7 @@ export class TkTreeView implements ComponentInterface {
     } else {
       const rootClasses = classNames('tk-tree-view', this.type, { disabled: this.disabled });
       return (
-        <div class={rootClasses}>
+        <div class={rootClasses} style={this.containerStyle}>
           <div class={classNames('tk-tree-view', 'content')}>{this.items.map((item, index) => this.renderItem(item, '', index))}</div>
         </div>
       );
