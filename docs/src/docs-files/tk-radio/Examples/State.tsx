020546487ab89084b1cb0ae6701065278ea8c3e9
Ulaş Turan <ulasturan06@hotmail.com>
2026-01-20 13:55:33 +0300

    fix(docs): correct radio and divider demos for clarity

--- docs/src/docs-files/tk-radio/Examples/State.tsx
+++ docs/src/docs-files/tk-radio/Examples/State.tsx
@@ -3,29 +3,29 @@ import FeatureDemo from '../../../components/FeatureDemo';
 import React from 'react';
 
 const State = () => {
-  const reactCode = `<TkRadioGroup invalid={true} label="Error">
+  const reactCode = `<TkRadioGroup invalid={true} label="Error" error="This is an error message">
   <TkRadio label="label 1" value="1" />
   <TkRadio label="label 2" value="2" />
 </TkRadioGroup>
-<TkRadioGroup invalid={true} label="Disabled">
+<TkRadioGroup label="Disabled" value="1">
   <TkRadio label="label 1" value="1" disabled />
   <TkRadio label="label 2" value="2" disabled />
 </TkRadioGroup>`;
 
-  const vueCode = `<TkRadioGroup :invalid="true" label="Error">
+  const vueCode = `<TkRadioGroup :invalid="true" label="Error" error="This is an error message">
   <TkRadio label="label 1" value="1" />
   <TkRadio label="label 2" value="2" />
 </TkRadioGroup>
-<TkRadioGroup :invalid="true" label="Disabled">
+<TkRadioGroup label="Disabled" value="1">
   <TkRadio label="label 1" value="1" disabled />
   <TkRadio label="label 2" value="2" disabled />
 </TkRadioGroup>`;
 
-  const angularCode = `<tk-radio-group [invalid]="true" label="Error">
+  const angularCode = `<tk-radio-group [invalid]="true" label="Error" error="This is an error message">
   <tk-radio label="label 1" value="1" />
   <tk-radio label="label 2" value="2" />
 </tk-radio-group>
-<tk-radio-group [invalid]="true" label="Disabled">
+<tk-radio-group label="Disabled" value="1">
   <tk-radio label="label 1" value="1" disabled />
   <tk-radio label="label 2" value="2" disabled />
 </tk-radio-group>`;
@@ -41,7 +41,7 @@ const State = () => {
         <TkRadio label="label 1" value="1" />
         <TkRadio label="label 2" value="2" />
       </TkRadioGroup>
-      <TkRadioGroup invalid={true} label="Disabled">
+      <TkRadioGroup label="Disabled" value="1">
         <TkRadio label="label 1" value="1" disabled />
         <TkRadio label="label 2" value="2" disabled />
       </TkRadioGroup>
