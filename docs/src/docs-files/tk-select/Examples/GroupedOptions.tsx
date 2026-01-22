08991cc2c6a87d7272e4bf370d3384bf34e8f587
nisaciko <cikohayrunnisa@gmail.com>
2026-01-22 23:09:43 +0300

    refactor(tk-select): Update vueCode syntax to remove .prop usage for options across examples

--- docs/src/docs-files/tk-select/Examples/GroupedOptions.tsx
+++ docs/src/docs-files/tk-select/Examples/GroupedOptions.tsx
@@ -52,7 +52,7 @@ const selectValue = ref();
     groupOptionsKey="options"
     optionLabelKey="label"
     optionValueKey="value"
-    :options.prop="[
+    :options="[
       {
         groupName: 'Marmara',
         options: [
