e229ccea45c28da04424a7162b9b556a1e80e1eb
nisaciko <cikohayrunnisa@gmail.com>
2026-01-22 13:44:26 +0300

    refactor(tk-alert): Clean up component structure and improve code consistency across examples

--- docs/src/docs-files/tk-alert/Examples/Messages.tsx
+++ docs/src/docs-files/tk-alert/Examples/Messages.tsx
@@ -60,30 +60,25 @@ const Messages = () => {
 `;
 
   const demo = (
-    <>
-      <div className="mb-4 flex flex-col gap-2">
-        <div className="title-h6">Single Message View</div>
-        <TkAlert variant="success" type="outlined" header="Success Filled Alert" message="Lorem Ipsum is simply dummy text of the printing and typesetting industry." />
-        <TkDivider />
-        <div className="title-h6">Multi Message View</div>
-        <TkAlert
-          variant="success"
-          type="outlined"
-          header="Success Filled Alert"
-          message={[
-            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
-            'Dummy text of the printing and typesetting',
-            "Lorem Ipsum has been the industry's standard.",
-          ]}
-        />
-      </div>
-    </>
+    <div className="mb-4 flex flex-col gap-2">
+      <div className="title-h6">Single Message View</div>
+      <TkAlert variant="success" type="outlined" header="Success Filled Alert" message="Lorem Ipsum is simply dummy text of the printing and typesetting industry." />
+      <TkDivider />
+      <div className="title-h6">Multi Message View</div>
+      <TkAlert
+        variant="success"
+        type="outlined"
+        header="Success Filled Alert"
+        message={[
+          'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
+          'Dummy text of the printing and typesetting',
+          "Lorem Ipsum has been the industry's standard.",
+        ]}
+      />
+    </div>
   );
-  return (
-    <>
-      <FeatureDemo demo={demo} reactCode={reactCode} vueCode={vueCode} angularCode={angularCode}></FeatureDemo>
-    </>
-  );
+
+  return <FeatureDemo demo={demo} reactCode={reactCode} vueCode={vueCode} angularCode={angularCode}></FeatureDemo>;
 };
 
 export default Messages;
--- docs/src/docs-files/tk-alert/Examples/Toaster.tsx
+++ docs/src/docs-files/tk-alert/Examples/Toaster.tsx
@@ -38,15 +38,12 @@ const Example = () => {
   };
 
   return (
-    <>
+    <div className="flex gap-2">
       <TkButton icon="north_west" variant="success" onTkClick={() => handleCreateToast('top-left', 'success')} />
-      &nbsp;
       <TkButton icon="north_east" variant="info" onTkClick={() => handleCreateToast('top-right', 'info')} />
-      &nbsp;
       <TkButton icon="south_west" variant="warning" onTkClick={() => handleCreateToast('bottom-left', 'warning')} />
-      &nbsp;
       <TkButton icon="south_east" variant="danger" onTkClick={() => handleCreateToast('bottom-right', 'danger')} />
-    </>
+    </div>
   );
 };
 
@@ -54,64 +51,62 @@ const Toaster = () => {
   const reactCode = `import { createToast } from "@takeoff-ui/core";
   
 const handleCreateToast = (position: string, variant: 'success' | 'warning' | 'info' | 'danger' | 'neutral') => {
-    createToast({
-      position: position,
-      header: "Deneme Başlık",
-      alignItems: 'start',
-      message: "Bu bir deneme mesajıdır. Dikkate almayınız.",
-      variant: variant,
-      type: "filled",
-      timeout: 10000,
-      removable: true,
-            actions: [
-        {
-          label: "Action 1",
-          icon: "flight",
-          type: "outlined",
-          variant: "secondary",
-          action: () => {
-            alert("action 1 clicked");
-          },
+  createToast({
+    position: position,
+    header: "Deneme Başlık",
+    alignItems: 'start',
+    message: "Bu bir deneme mesajıdır. Dikkate almayınız.",
+    variant: variant,
+    type: "outlined",
+    timeout: 10000,
+    removable: true,
+    actions: [
+      {
+        label: "Action 1",
+        icon: "flight",
+        type: "outlined",
+        variant: "secondary",
+        action: () => {
+          alert("action 1 clicked");
         },
-        {
-          label: "Action 2",
-          icon: "flight",
-          type: "filled",
-          variant: "primary",
-          action: () => {
-            alert("action 2 clicked");
-          },
+      },
+      {
+        label: "Action 2",
+        icon: "flight",
+        type: "filled",
+        variant: "primary",
+        action: () => {
+          alert("action 2 clicked");
         },
-      ],
-    });
-  };
-  return (
-    <>
-      <TkButton
-        icon="north_west"
-        variant="success"
-        onTkClick={() => handleCreateToast("top-left", "success")}
-      />
-      &nbsp;
-      <TkButton
-        icon="north_east"
-        variant="info"
-        onTkClick={() => handleCreateToast("top-right", "info")}
-      />
-         &nbsp;
-      <TkButton
-        icon="south_west"
-        variant="warning"
-        onTkClick={() => handleCreateToast("bottom-left", "warning")}
-      />
-      &nbsp;
-      <TkButton
-        icon="south_east"
-        variant="danger"
-        onTkClick={() => handleCreateToast("bottom-right", "danger")}
-      />
-    </>
-  );`;
+      },
+    ],
+  });
+};
+
+return (
+  <div className="flex gap-2">
+    <TkButton
+      icon="north_west"
+      variant="success"
+      onTkClick={() => handleCreateToast("top-left", "success")}
+    />
+    <TkButton
+      icon="north_east"
+      variant="info"
+      onTkClick={() => handleCreateToast("top-right", "info")}
+    />
+    <TkButton
+      icon="south_west"
+      variant="warning"
+      onTkClick={() => handleCreateToast("bottom-left", "warning")}
+    />
+    <TkButton
+      icon="south_east"
+      variant="danger"
+      onTkClick={() => handleCreateToast("bottom-right", "danger")}
+    />
+  </div>
+);`;
 
   const vueCode = `<script setup>
 import { TkButton } from '@takeoff-ui/vue';
@@ -124,108 +119,107 @@ const handleCreateToast = (position, variant) => {
     alignItems: 'start',
     message: "Bu bir deneme mesajıdır. Dikkate almayınız.",
     variant: variant,
-    type: "filled",
+    type: "outlined",
     timeout: 10000,
     removable: true,
-          actions: [
-        {
-          label: "Action 1",
-          icon: "flight",
-          type: "outlined",
-          variant: "secondary",
-          action: () => {
-            alert("action 1 clicked");
-          },
+    actions: [
+      {
+        label: "Action 1",
+        icon: "flight",
+        type: "outlined",
+        variant: "secondary",
+        action: () => {
+          alert("action 1 clicked");
         },
-        {
-          label: "Action 2",
-          icon: "flight",
-          type: "filled",
-          variant: "primary",
-          action: () => {
-            alert("action 2 clicked");
-          },
+      },
+      {
+        label: "Action 2",
+        icon: "flight",
+        type: "filled",
+        variant: "primary",
+        action: () => {
+          alert("action 2 clicked");
         },
-      ],
+      },
+    ],
   });
 };
-
-
 </script>
 
 <template>
-  <TkButton icon="north_west" variant="success" @tkClick="handleCreateToast('top-left', 'success')" />
-  <TkButton icon="north_east" variant="info" @tkClick="handleCreateToast('top-right', 'info')" />
-  <TkButton icon="south_west" variant="warning" @tkClick="handleCreateToast('bottom-left', 'warning')" />
-  <TkButton icon="south_east" variant="danger" @tkClick="handleCreateToast('bottom-right', 'danger')" />
+  <div class="flex gap-2">
+    <TkButton icon="north_west" variant="success" @tkClick="handleCreateToast('top-left', 'success')" />
+    <TkButton icon="north_east" variant="info" @tkClick="handleCreateToast('top-right', 'info')" />
+    <TkButton icon="south_west" variant="warning" @tkClick="handleCreateToast('bottom-left', 'warning')" />
+    <TkButton icon="south_east" variant="danger" @tkClick="handleCreateToast('bottom-right', 'danger')" />
+  </div>
 </template>
 `;
 
   const angularCode = `import { Component } from '@angular/core';
 import { createToast } from '@takeoff-ui/core';
-  
-  @Component({
-    selector: 'app-root',
-    template: \`
+
+@Component({
+  selector: 'app-root',
+  template: \`
+    <div class="flex gap-2">
       <tk-button
         icon="north_west"
         variant="success"
-        (tk-click)="handleCreateToast('top-left', 'success')"
+        (tkClick)="handleCreateToast('top-left', 'success')"
       ></tk-button>
-      &nbsp;
       <tk-button
         icon="north_east"
         variant="info"
-        (tk-click)="handleCreateToast('top-right', 'info')"
+        (tkClick)="handleCreateToast('top-right', 'info')"
       ></tk-button>
-      &nbsp;
       <tk-button
         icon="south_west"
         variant="warning"
-        (tk-click)="handleCreateToast('bottom-left', 'warning')"
+        (tkClick)="handleCreateToast('bottom-left', 'warning')"
       ></tk-button>
-      &nbsp;
       <tk-button
         icon="south_east"
         variant="danger"
-        (tk-click)="handleCreateToast('bottom-right', 'danger')"
+        (tkClick)="handleCreateToast('bottom-right', 'danger')"
       ></tk-button>
-    \`,
-  })
-  export class AppComponent {
-    handleCreateToast(position: string, variant: 'success' | 'warning' | 'info' | 'danger' | 'neutral') {
-      createToast({
-        position,
-        header: 'Deneme Başlık',
-        alignItems: 'start',
-        message: 'Bu bir deneme mesajıdır. Dikkate almayınız.',
-        variant: variant,
-        type: 'filled',
-        timeout: 10000,
-        removable: true,
-        actions: [
-          {
-            label: 'Action 1',
-            icon: 'flight',
-            type: 'outlined',
-            variant: 'secondary',
-            action: () => {
-              alert('action 1 clicked');
-            },
+    </div>
+  \`,
+})
+export class AppComponent {
+  handleCreateToast(position: string, variant: 'success' | 'warning' | 'info' | 'danger' | 'neutral') {
+    createToast({
+      position,
+      header: 'Deneme Başlık',
+      alignItems: 'start',
+      message: 'Bu bir deneme mesajıdır. Dikkate almayınız.',
+      variant: variant,
+      type: 'outlined',
+      timeout: 10000,
+      removable: true,
+      actions: [
+        {
+          label: 'Action 1',
+          icon: 'flight',
+          type: 'outlined',
+          variant: 'secondary',
+          action: () => {
+            alert('action 1 clicked');
           },
-          {
-            label: 'Action 2',
-            icon: 'flight',
-            type: 'filled',
-            variant: 'primary',
-            action: () => {
-              alert('action 2 clicked');
-            },
+        },
+        {
+          label: 'Action 2',
+          icon: 'flight',
+          type: 'filled',
+          variant: 'primary',
+          action: () => {
+            alert('action 2 clicked');
           },
-        ],
-      });
-    }
-  }`;
+        },
+      ],
+    });
+  }
+}`;
 
   const demo = <Example />;
 
