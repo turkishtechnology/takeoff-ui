e7483781137ec897fb1521494ad643f902b48bd2
nisaciko <cikohayrunnisa@gmail.com>
2026-01-22 13:31:59 +0300

    refactor(tk-accordion): Update component props and structure for consistency across examples

--- docs/src/docs-files/tk-accordion/Examples/Position.tsx
+++ docs/src/docs-files/tk-accordion/Examples/Position.tsx
@@ -1,39 +1,84 @@
 import { TkAccordion, TkAccordionItem } from '@takeoff-ui/react';
-
 import FeatureDemo from '../../../components/FeatureDemo';
 import React from 'react';
 
 const Position = () => {
-  const reactCode = `<TkAccordion arrow-position="left">
-    <TkAccordionItem size="base" icon="flight">
-      <span slot="header">Panel 1 Title</span>
-      <div slot="content">Panel 1 Content</div>
-    </TkAccordionItem>
-    <TkAccordionItem icon="flight">
-      <span slot="header">Panel 2 Title</span>
-      <div slot="content">Panel 2 Content</div>
-    </TkAccordionItem>
-    <TkAccordionItem icon="travel">
-      <span slot="header">Panel 3 Title</span>
-      <div slot="content">Panel 3 Content</div>
-    </TkAccordionItem>
-  </TkAccordion>`;
-  const vueCode = `<TkAccordion arrow-position="left">
-    <TkAccordionItem size="base" icon="flight">
-      <span slot="header">Panel 1 Title</span>
-      <div slot="content">Panel 1 Content</div>
-    </TkAccordionItem>
-    <TkAccordionItem icon="flight">
-      <span slot="header">Panel 2 Title</span>
-      <div slot="content">Panel 2 Content</div>
-    </TkAccordionItem>
-    <TkAccordionItem icon="travel">
-      <span slot="header">Panel 3 Title</span>
-      <div slot="content">Panel 3 Content</div>
-    </TkAccordionItem>
-  </TkAccordion>`;
+  const reactCode = `<TkAccordion>
+  <TkAccordionItem size="base" icon="flight">
+    <span slot="header">Panel 1 Title</span>
+    <div slot="content">Panel 1 Content</div>
+  </TkAccordionItem>
+  <TkAccordionItem icon="flight">
+    <span slot="header">Panel 2 Title</span>
+    <div slot="content">Panel 2 Content</div>
+  </TkAccordionItem>
+  <TkAccordionItem icon="travel">
+    <span slot="header">Panel 3 Title</span>
+    <div slot="content">Panel 3 Content</div>
+  </TkAccordionItem>
+</TkAccordion>
 
-  const angularCode = `<tk-accordion arrow-position="left">
+<TkAccordion arrowPosition="left">
+  <TkAccordionItem size="base" icon="flight">
+    <span slot="header">Panel 1 Title</span>
+    <div slot="content">Panel 1 Content</div>
+  </TkAccordionItem>
+  <TkAccordionItem icon="flight">
+    <span slot="header">Panel 2 Title</span>
+    <div slot="content">Panel 2 Content</div>
+  </TkAccordionItem>
+  <TkAccordionItem icon="travel">
+    <span slot="header">Panel 3 Title</span>
+    <div slot="content">Panel 3 Content</div>
+  </TkAccordionItem>
+</TkAccordion>`;
+
+  const vueCode = `<TkAccordion>
+  <TkAccordionItem size="base" icon="flight">
+    <span slot="header">Panel 1 Title</span>
+    <div slot="content">Panel 1 Content</div>
+  </TkAccordionItem>
+  <TkAccordionItem icon="flight">
+    <span slot="header">Panel 2 Title</span>
+    <div slot="content">Panel 2 Content</div>
+  </TkAccordionItem>
+  <TkAccordionItem icon="travel">
+    <span slot="header">Panel 3 Title</span>
+    <div slot="content">Panel 3 Content</div>
+  </TkAccordionItem>
+</TkAccordion>
+
+<TkAccordion arrowPosition="left">
+  <TkAccordionItem size="base" icon="flight">
+    <span slot="header">Panel 1 Title</span>
+    <div slot="content">Panel 1 Content</div>
+  </TkAccordionItem>
+  <TkAccordionItem icon="flight">
+    <span slot="header">Panel 2 Title</span>
+    <div slot="content">Panel 2 Content</div>
+  </TkAccordionItem>
+  <TkAccordionItem icon="travel">
+    <span slot="header">Panel 3 Title</span>
+    <div slot="content">Panel 3 Content</div>
+  </TkAccordionItem>
+</TkAccordion>`;
+
+  const angularCode = `<tk-accordion>
+  <tk-accordion-item size="base" icon="flight">
+    <span slot="header">Panel 1 Title</span>
+    <div slot="content">Panel 1 Content</div>
+  </tk-accordion-item>
+  <tk-accordion-item icon="flight">
+    <span slot="header">Panel 2 Title</span>
+    <div slot="content">Panel 2 Content</div>
+  </tk-accordion-item>
+  <tk-accordion-item icon="travel">
+    <span slot="header">Panel 3 Title</span>
+    <div slot="content">Panel 3 Content</div>
+  </tk-accordion-item>
+</tk-accordion>
+
+<tk-accordion arrow-position="left">
   <tk-accordion-item size="base" icon="flight">
     <span slot="header">Panel 1 Title</span>
     <div slot="content">Panel 1 Content</div>
@@ -49,10 +94,10 @@ const Position = () => {
 </tk-accordion>`;
 
   const demo = (
-    <>
-      <div className="container">
-        <div>Right Positioned Arrows</div>
-        <TkAccordion allow-multiple="false">
+    <div className="flex flex-col gap-4">
+      <div>
+        <div className="mb-2">Right Positioned Arrows</div>
+        <TkAccordion allowMultiple={false}>
           <TkAccordionItem size="base" icon="flight">
             <span slot="header">Panel 1 Title</span>
             <div slot="content">Panel 1 Content</div>
@@ -67,11 +112,10 @@ const Position = () => {
           </TkAccordionItem>
         </TkAccordion>
       </div>
-      <br />
 
-      <div className="container">
-        <div>Left Positioned Arrows</div>
-        <TkAccordion allow-multiple="false" arrow-position="left">
+      <div>
+        <div className="mb-2">Left Positioned Arrows</div>
+        <TkAccordion allowMultiple={false} arrowPosition="left">
           <TkAccordionItem size="base" icon="flight">
             <span slot="header">Panel 1 Title</span>
             <div slot="content">Panel 1 Content</div>
@@ -86,14 +130,10 @@ const Position = () => {
           </TkAccordionItem>
         </TkAccordion>
       </div>
-    </>
+    </div>
   );
 
-  return (
-    <>
-      <FeatureDemo demo={demo} reactCode={reactCode} vueCode={vueCode} angularCode={angularCode}></FeatureDemo>
-    </>
-  );
+  return <FeatureDemo demo={demo} reactCode={reactCode} vueCode={vueCode} angularCode={angularCode}></FeatureDemo>;
 };
 
 export default Position;
