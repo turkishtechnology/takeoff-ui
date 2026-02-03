9258876d11f1ce90e4d8c7e71565d8da0f384b45
Ulaş Turan <ulasturan06@hotmail.com>
2026-02-03 15:02:27 +0300

    chore: remove trailing blank line

--- packages/core/src/global/types.ts
+++ packages/core/src/global/types.ts
@@ -2,4 +2,3 @@
  * Represents CSS style properties as strings.
  */
 export type CSSStyleProperties = Partial<{ [K in keyof CSSStyleDeclaration as CSSStyleDeclaration[K] extends string ? K : never]: string }>;
-
