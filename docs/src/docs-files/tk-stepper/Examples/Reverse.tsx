403d127688dec0b000358fa1687aa338166d1507
Pınar Yalçınduran <104770005+pinaryalcinduran@users.noreply.github.com>
2026-02-04 19:28:29 +0300

    feat(tk-stepper): Add Reverse example and documentation

--- docs/src/docs-files/tk-stepper/Examples/Reverse.tsx
+++ docs/src/docs-files/tk-stepper/Examples/Reverse.tsx
@@ -2,65 +2,52 @@ import React from 'react';
 import { TkStepper, TkStep, TkDivider } from '@takeoff-ui/react';
 import FeatureDemo from '../../../components/FeatureDemo';
 
-const Size = () => {
-  const reactCode = `<TkStepper size="small">
+const Reverse = () => {
+  const reactCode = `<TkStepper reverse>
   <TkStep header="General Information" subheader="Basic campaign details" complete />
   <TkStep header="Category Details" subheader="Campaign categorization" complete />
-  <TkStep header="Communication" subheader="Communication strategies" isActive />
-  <TkStep header="Summary" subheader="Campaign overview" />
 </TkStepper>
-<TkStepper size="large">
+
+<TkStepper orientation="vertical" reverse>
   <TkStep header="General Information" subheader="Basic campaign details" complete />
   <TkStep header="Category Details" subheader="Campaign categorization" complete />
-  <TkStep header="Communication" subheader="Communication strategies" isActive />
-  <TkStep header="Summary" subheader="Campaign overview" />
 </TkStepper>`;
 
-  const vueCode = `<tk-stepper size="small">
+  const vueCode = `<tk-stepper reverse>
   <tk-step header="General Information" subheader="Basic campaign details" complete />
   <tk-step header="Category Details" subheader="Campaign categorization" complete />
-  <tk-step header="Communication" subheader="Communication strategies" is-active />
-  <tk-step header="Summary" subheader="Campaign overview" />
 </tk-stepper>
-<tk-stepper size="large">
+
+<tk-stepper orientation="vertical" reverse>
   <tk-step header="General Information" subheader="Basic campaign details" complete />
   <tk-step header="Category Details" subheader="Campaign categorization" complete />
-  <tk-step header="Communication" subheader="Communication strategies" is-active />
-  <tk-step header="Summary" subheader="Campaign overview" />
 </tk-stepper>`;
 
-  const angularCode = `<tk-stepper size="small">
+  const angularCode = `<tk-stepper reverse>
   <tk-step header="General Information" subheader="Basic campaign details" complete></tk-step>
   <tk-step header="Category Details" subheader="Campaign categorization" complete></tk-step>
-  <tk-step header="Communication" subheader="Communication strategies" isActive></tk-step>
-  <tk-step header="Summary" subheader="Campaign overview"></tk-step>
 </tk-stepper>
-<tk-stepper size="large">
+
+<tk-stepper orientation="vertical" reverse>
   <tk-step header="General Information" subheader="Basic campaign details" complete></tk-step>
   <tk-step header="Category Details" subheader="Campaign categorization" complete></tk-step>
-  <tk-step header="Communication" subheader="Communication strategies" isActive></tk-step>
-  <tk-step header="Summary" subheader="Campaign overview"></tk-step>
 </tk-stepper>`;
 
   const demo = (
     <div className=" m-4">
       <div className="h-48">
-        <p className="justify-self-center font-medium">Small</p>
-        <TkStepper size="small">
+        <p className="justify-self-center font-medium">Horizontal Reverse</p>
+        <TkStepper reverse>
           <TkStep header="General Information" subheader="Basic campaign details" complete />
           <TkStep header="Category Details" subheader="Campaign categorization" complete />
-          <TkStep header="Communication" subheader="Communication strategies" isActive />
-          <TkStep header="Summary" subheader="Campaign overview" />
         </TkStepper>
       </div>
       <TkDivider />
       <div className="h-48">
-        <p className="justify-self-center font-medium">Large</p>
-        <TkStepper size="large">
+        <p className="justify-self-center font-medium">Vertical Reverse</p>
+        <TkStepper orientation="vertical" reverse>
           <TkStep header="General Information" subheader="Basic campaign details" complete />
           <TkStep header="Category Details" subheader="Campaign categorization" complete />
-          <TkStep header="Communication" subheader="Communication strategies" isActive />
-          <TkStep header="Summary" subheader="Campaign overview" />
         </TkStepper>
       </div>
     </div>
@@ -69,4 +56,4 @@ const Size = () => {
   return <FeatureDemo demo={demo} reactCode={reactCode} vueCode={vueCode} angularCode={angularCode} />;
 };
 
-export default Size;
+export default Reverse;
