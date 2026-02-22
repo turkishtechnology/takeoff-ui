7dd4d10a38bd3d6e5512225de1167bcda9b2718d
nisaciko <cikohayrunnisa@gmail.com>
2026-02-22 23:11:56 +0300

    fix(error-message): Update error handling styles and classes across various components

--- packages/core/src/components/tk-input/tk-input.tsx
+++ packages/core/src/components/tk-input/tk-input.tsx
@@ -641,7 +641,7 @@ export class TkInput implements ComponentInterface {
       const hintIcon = <tk-icon {...getIconElementProps('info')} />;
 
       hint = (
-        <span class="hint">
+        <span class="hint error">
           {hintIcon}
           {this.error}
         </span>
