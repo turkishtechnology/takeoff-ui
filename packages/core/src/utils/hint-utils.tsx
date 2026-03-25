ca1986f9c7d1c17558653d40e6aa814074fefd46
Pınar Yalçınduran <104770005+pinaryalcinduran@users.noreply.github.com>
2026-03-25 13:47:07 +0300

    refactor(hint): Update styles and improve renderHint function

--- packages/core/src/utils/hint-utils.tsx
+++ packages/core/src/utils/hint-utils.tsx
@@ -7,15 +7,18 @@ import classNames from 'classnames';
  * @param params - ( hint?: string, error?: string, invalid?: boolean )
  * @returns JSX.Element| null
  */
-export function renderHint(hint?: string, error?: string, invalid?: boolean): JSX.Element | null {
+export function renderHint(hint?: string, error?: string, invalid?: boolean): HTMLDivElement | null {
   const isError = error && error.length > 0;
   const isHint = hint && hint.length > 0;
   if (!isError && !isHint) {
     return null;
   }
+  const icon = isError || invalid ? 'error' : 'info';
+
   return (
     <div class={classNames('tk-hint-wrapper', { invalid: invalid, error: isError })}>
-      <tk-icon {...getIconElementProps('info', { color: isError || invalid ? 'var(--states-danger-base)' : 'var(--text-base)' })} /> <span>{isError ? error : hint}</span>
+      <tk-icon {...getIconElementProps(icon, { color: isError || invalid ? 'var(--states-danger-base)' : 'var(--icon-base)', size: 'small' })} />
+      <span>{isError ? error : hint}</span>
     </div>
   );
 }
