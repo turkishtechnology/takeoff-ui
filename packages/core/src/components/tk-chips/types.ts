a40eeaba1e2cc775dd9391357cb9fc3caaf107aa
nisaciko <cikohayrunnisa@gmail.com>
2026-04-02 21:39:12 +0300

    feat(tk-chips): Add iconPosition prop to control icon placement

--- packages/core/src/components/tk-chips/types.ts
+++ packages/core/src/components/tk-chips/types.ts
@@ -3,6 +3,7 @@ import { CSSStyleProperties } from '../../global/types';
 
 export interface IChipOptions {
   icon?: string | IIconOptions;
+  iconPosition?: 'left' | 'right';
   type?: 'filled' | 'filledlight' | 'outlined' | 'avatar';
   size?: 'large' | 'base' | 'small';
   variant?: 'primary' | 'secondary' | 'neutral' | 'info' | 'success' | 'danger' | 'warning' | 'verified';
