6ec8e6ad9392dbd8b0181073858fbed1fb606ac2
pinaryalcinduran <pinarylcndrn@hotmail.com>
2026-07-01 17:31:11 +0300

    chore(release): Version packages to 0.11.10

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -98,7 +98,7 @@ const config: Config = {
         },
         {
           to: 'https://github.com/turkishtechnology/takeoff-ui/releases',
-          label: 'v0.11.8',
+          label: 'v0.11.10',
           position: 'right',
         },
         {
