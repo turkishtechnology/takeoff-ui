7d0e8f5575d0efd3101d1e477f22624a36527e3f
pinaryalcinduran <pinarylcndrn@hotmail.com>
2026-07-17 10:30:12 +0300

    feat(datepicker): Update styles

--- packages/core/src/components/tk-datepicker/tk-datepicker.tsx
+++ packages/core/src/components/tk-datepicker/tk-datepicker.tsx
@@ -2234,6 +2234,7 @@ export class TkDatePicker {
     if (!this.isOpen && !this.inline) return null;
     const panelClasses = classNames('tk-datepicker-panel', `tk-datepicker-panel-${this.size}`, {
       'tk-datepicker-panel-inline': this.inline,
+      'tk-datepicker-panel-with-footer': this.hasFooterSlot || this.hasFooterActionsSlot,
     });
     const bodyClasses = classNames('tk-datepicker-body', {
       'tk-datepicker-months-view': this.currentView === 'months',
